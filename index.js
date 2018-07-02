74b1dcfd0b5d26542ba242f430c311e9faceee69
xxtom00 <34427163+xxtom00@users.noreply.github.com>
2018-07-02 13:43:38 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
 
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+  if (msg.content === 'ping') {
+    msg.reply('Pong!');
+  }
+});
+
+client.login('NDYxMjYxMjk0NDE1MzgwNDkw.DhuvYQ.8yOcRH9jx-r2V3qONGzxm-VMtV4');
